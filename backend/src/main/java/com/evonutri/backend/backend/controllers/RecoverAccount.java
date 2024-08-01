5f1ef72cd02d18197e1e999e990ce3b72c5a15d6
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-08-01 10:52:21 -0300

    Added classes

---
+++ backend/src/main/java/com/evonutri/backend/backend/controllers/RecoverAccount.java
@@ -0,0 +1,18 @@
+package com.evonutri.backend.backend.controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecoverAccount {
+    public void recuperarAcessoPorEmail() {
+        // Implementation
+    }
+
+    public void verificarCpf() {
+        // Implementation
+    }
+
+    public void verificarContato() {
+        // Implementation
+    }
+}
