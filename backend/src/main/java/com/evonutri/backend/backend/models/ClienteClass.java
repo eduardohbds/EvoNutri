eb7901ac191afaeb9cc6be3e514d622d27b6d8d5
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-08-01 08:50:19 -0300

    Added models Cliente and Nutri

---
+++ backend/src/main/java/com/evonutri/backend/backend/models/ClienteClass.java
@@ -0,0 +1,7 @@
+package com.evonutri.backend.backend.models;
+
+public class ClienteClass {
+    String email;
+    String cpf;
+    String contato;
+}
---
+++ backend/src/main/java/com/evonutri/backend/backend/models/NutricionistaClass.java
@@ -0,0 +1,8 @@
+package com.evonutri.backend.backend.models;
+
+public class NutricionistaClass {
+    String crnm;
+    boolean tipo;
+    String zona;
+}
+
