608e291fe2755bbe942e8c79ce715ee86a1f4bb9
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-08-01 09:44:01 -0300

    chore: added Spring Web

--- backend/src/main/java/com/evonutri/backend/backend/models/ClienteClass.java
+++ backend/src/main/java/com/evonutri/backend/backend/models/ClienteClass.java
@@ -1,5 +1,10 @@
 package com.evonutri.backend.backend.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ClienteClass {
     String email;
     String cpf;
--- backend/src/main/java/com/evonutri/backend/backend/models/NutricionistaClass.java
+++ backend/src/main/java/com/evonutri/backend/backend/models/NutricionistaClass.java
@@ -1,5 +1,10 @@
 package com.evonutri.backend.backend.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class NutricionistaClass {
     String crnm;
     boolean tipo;
